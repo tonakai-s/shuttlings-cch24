ba4f9a10ebeaf52566a21785f99d2a73e12f5131
tonakai-s <rdorneles64@gmail.com>
2024-12-22 23:13:25 -0300

    Finish day 16 tasks

--- src/challenges/day_16.rs
+++ src/challenges/day_16.rs
@@ -1,50 +1,70 @@
 use actix_web::{web, Scope};
 
 mod gift {
-    use actix_web::{cookie::Cookie, get, post, web, HttpRequest, HttpResponse};
-    use jsonwebtoken::{decode, encode, Algorithm, DecodingKey, EncodingKey, Header, Validation};
-    use serde::{Deserialize, Serialize};
+    use std::collections::{HashMap, HashSet};
 
-    #[derive(Debug, Deserialize, Serialize)]
-    struct Gift {
-        content: String,
-        exp: u64
-    }
-    impl Gift {
-        fn new(content: String) -> Self {
-            Gift {
-                content,
-                exp: 10000000000
+    use actix_web::{cookie::Cookie, get, post, web, HttpRequest, HttpResponse};
+    use jsonwebtoken::{decode, encode, errors::ErrorKind, Algorithm, DecodingKey, EncodingKey, Header, Validation};
+    use serde_json::Value;
+
+    type Gift = HashMap<String, Value>;
+
+    #[post("/wrap")]
+    async fn pack(jwt_secret: web::Data<Vec<u8>>, bytes: web::Bytes) -> HttpResponse {
+        let encoding_key = EncodingKey::from_secret(jwt_secret.as_slice());
+        let json_gift: Gift = serde_json::from_slice::<Gift>(&bytes)
+            .expect("Unable to parse the gift on pack.");
+
+        match encode(&Header::default(), &json_gift, &encoding_key) {
+            Err(e) => {
+                println!("Error on encoding the gift: {}", e);
+                HttpResponse::InternalServerError().finish()
             }
+            Ok(gift) => HttpResponse::Ok().cookie(Cookie::new("gift", gift)).finish()
         }
     }
 
-    #[post("/wrap")]
-    async fn pack(bytes: web::Bytes) -> HttpResponse {
-        const SECRET: &str = "Test";
-        let json = Gift::new(String::from_utf8(bytes.to_vec()).unwrap());
-        let token = encode(&Header::default(), &json, &EncodingKey::from_secret(SECRET.as_ref())).unwrap();
-        let decoded = decode::<Gift>(&token, &DecodingKey::from_secret(&SECRET.as_ref()), &Validation::default());
-        println!("Decode on pack: {:?}", decoded);
-        HttpResponse::Ok().cookie(Cookie::new("gift", token)).finish()
-    }
-
     #[get("/unwrap")]
-    async fn unpack(req: HttpRequest) -> HttpResponse {
+    async fn unpack(jwt_secret: web::Data<Vec<u8>>, req: HttpRequest) -> HttpResponse {
         let gift_jwt = req.cookie("gift");
         if gift_jwt.is_none() {
             return HttpResponse::BadRequest().finish();
         }
 
-        const SECRET: &str = "Test";
-        println!("Received JWT: {:?}", gift_jwt.clone().unwrap().to_string().replace("gift=", ""));
-        let token = decode::<Gift>(&gift_jwt.unwrap().to_string(), &DecodingKey::from_secret(&SECRET.as_ref()), &Validation::default());
-        if token.is_err() {
-            println!("Message err: {:?}", token);
-            return HttpResponse::InternalServerError().finish();
-        }
+        let jwt_token = gift_jwt.unwrap().to_string().split("=").skip(1).collect::<String>();
+        let decoding_key = DecodingKey::from_secret(jwt_secret.as_slice());
 
-        HttpResponse::Ok().body(token.unwrap().claims.content)
+        let mut validation = Validation::default();
+        validation.validate_exp = false;
+        validation.required_spec_claims = HashSet::from(["".to_string()]);
+
+        match decode::<Gift>(&jwt_token, &decoding_key, &validation) {
+            Err(e) => {
+                println!("Message err: {:?}", e);
+                return HttpResponse::InternalServerError().finish();
+            }
+            Ok(gift) => HttpResponse::Ok().json(gift.claims)
+        }
+    }
+
+    #[post("/decode")]
+    async fn unpack_olders(jwt_secret: web::Data<Vec<u8>>, bytes: web::Bytes) -> HttpResponse {
+        let token = String::from_utf8(bytes.to_vec()).unwrap();
+        let decoding_key = DecodingKey::from_rsa_pem(jwt_secret.as_slice())
+            .expect("Unable to generate decode key from PEM.");
+
+        let mut validation = Validation::default();
+        validation.algorithms = vec![Algorithm::RS256, Algorithm::RS512];
+        validation.validate_exp = false;
+        validation.required_spec_claims = HashSet::from(["".to_string()]);
+
+        match decode::<Gift>(&token, &decoding_key, &validation) {
+            Ok(gift) => HttpResponse::Ok().json(gift.claims),
+            Err(e) => match e.kind() {
+                ErrorKind::InvalidSignature => HttpResponse::Unauthorized().finish(),
+                _ => HttpResponse::BadRequest().finish()
+            }
+        }
     }
 }
 
@@ -52,4 +72,5 @@ pub fn scope() -> Scope {
     web::scope("/16")
         .service(gift::pack)
         .service(gift::unpack)
+        .service(gift::unpack_olders)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
-use std::{sync::Mutex, time::Duration};
+use std::{fs::{self,  File}, io::Read, sync::Mutex, time::Duration};
 
 use leaky_bucket::RateLimiter;
+use shuttle_runtime::SecretStore;
 use shuttlings_cch24::challenges::{self, day_12::Board, day_9::MilkBucket};
 
 use actix_web::web::{self, ServiceConfig};
 use shuttle_actix_web::ShuttleActixWeb;
 
 #[shuttle_runtime::main]
-async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
+async fn main(#[shuttle_runtime::Secrets] secrets: SecretStore) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
+    let jwt_secret = secrets.get("PUB_PEM").expect("Unable to read PUB_PEM secret").as_bytes().to_vec();
+
     let milk_bucket = web::Data::new(MilkBucket {
         bucket: Mutex::new(RateLimiter::builder().max(5).initial(5).interval(Duration::from_secs(1)).build())
     });
@@ -19,7 +22,7 @@ async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clon
         cfg.service(challenges::day_5::scope());
         cfg.service(challenges::day_9::scope()).app_data(milk_bucket);
         cfg.service(challenges::day_12::scope()).app_data(milk_cookie_board);
-        cfg.service(challenges::day_16::scope());
+        cfg.service(challenges::day_16::scope()).app_data(web::Data::new(jwt_secret));
     };
 
     Ok(config.into())
